1bf7c8ea02315f298e2d6b83bbf471ceb1a9039a
Theosaurus-Rex <theosaurus13@gmail.com>
2025-01-10 13:06:26 +1100

    style: Add theme extensions for design colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-green": "#C4F82A",
+        "secondary-charcoal": {
+          dark: "#141414",
+          medium: "#1F1F1F",
+          light: "#333333",
+        },
+      },
+    },
   },
   plugins: [],
 };
